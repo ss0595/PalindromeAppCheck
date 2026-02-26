0e414497662ed45e3aa34c03d98ee8b3ae08fc8c
ss0595 <sahusanskarsahu12@gmail.com>
2026-02-26 16:16:59 +0530

    UC2:Hardcoded palindrome

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,16 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = checkPalindrome(input);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+
+    public static boolean checkPalindrome(String text) {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+}
